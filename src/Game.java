94b464902e9bc7875e1958d270cb652f045c60df
Petr Liszok <liszok@email.cz>
2025-06-05 09:52:44 +0200

    2D ray casting works

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
 
 public class Game implements Runnable, MouseMotionListener {
 
@@ -15,6 +19,7 @@ public class Game implements Runnable, MouseMotionListener {
 
     public Game() {
         Maze mazeLevel1 = new Maze(Level.loadLevel(1));
+        this.maze = mazeLevel1;
 
         int gridCols = mazeLevel1.getColCount();
         int gridRows = mazeLevel1.getRowCount();
@@ -28,11 +33,75 @@ public class Game implements Runnable, MouseMotionListener {
 
     private void setupWindow(){
         JFrame frame = new JFrame("Game - Level 1");
-        frame.getDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(canvas = new Canvas());
-        frame.addMouseMotionListener(this);
+        canvas.addMouseMotionListener(this);
         frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void run(){
+        while (true){
+            render();
+            try {
+                Thread.sleep(16);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void render(){
+        BufferStrategy bs = canvas.getBufferStrategy();
+        if (bs==null){
+            canvas.createBufferStrategy(2); //creates off screen buffer and swaps it
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        drawMaze(g);
+        drawRays(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    private void drawMaze(Graphics g){
+        for(int row = 0; row < maze.getRowCount(); row++){
+            for(int col = 0; col < maze.getColCount(); col++){
+                if(maze.getCell(row,col) == 1){
+                    g.setColor(Color.GRAY);
+                }
+                else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(col*cellSize, row*cellSize, cellSize, cellSize);
+            }
+        }
+    }
+
+    private void drawRays(Graphics g){
+        LinkedList<Line2D.Float> rays = rayCaster.castRays(mouseX, mouseY, 360, 150);
+        g.setColor(Color.GREEN);
+        for (Line2D.Float ray : rays){
+            g.drawLine((int) ray.x1, (int) ray.y1, (int) ray.x2, (int) ray.y2);
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e){
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e){
+        mouseMoved(e);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        Maze maze = new Maze(Level.loadLevel(1));
-
-        System.out.println(maze);
+        Game game = new Game();
     }
 }
